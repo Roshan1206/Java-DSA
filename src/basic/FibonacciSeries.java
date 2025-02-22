99de6d8b04b837fa0fcce9c4a072beed96f3808e
roshan1206 <sahuroshan96@gmail.com>
2025-02-22 18:31:10 +0530

    more

--- src/basic/FibonacciSeries.java
+++ src/basic/FibonacciSeries.java
@@ -1,4 +1,22 @@
 package basic;
 
 public class FibonacciSeries {
+    public static void main(String[] args) {
+        printFibonnaciSeries(10);
+    }
+
+    public static void printFibonnaciSeries(int number){
+        int firstNumber = 0;
+        int secondNumber = 1;
+
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+
+        for (int i = 0; i < number-2; i++) {
+            int nextNumber = firstNumber + secondNumber;
+            System.out.println(nextNumber);
+            firstNumber = secondNumber;
+            secondNumber = nextNumber;
+        }
+    }
 }
--- src/basic/Palindrome.java
+++ src/basic/Palindrome.java
@@ -1,4 +1,18 @@
 package basic;
 
+import java.util.Arrays;
+
 public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("Hello World"));
+        System.out.println(isPalindrome("abcba"));
+        System.out.println(isPalindrome("12321"));
+    }
+
+    public static boolean isPalindrome(String string){
+//        String reverseString = Arrays.stream(string.split("")).toList().reversed().toString();
+        StringBuilder reverseString = new StringBuilder(string).reverse();
+//        return string.equalsIgnoreCase(reverseString);
+        return string.equalsIgnoreCase(String.valueOf(reverseString));
+    }
 }
